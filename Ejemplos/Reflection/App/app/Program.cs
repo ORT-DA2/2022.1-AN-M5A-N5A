e6a343f7035a2080793d87d6f7d1358f62fd6078
naickit <1nico.blanco@gmail.com>
2022-04-28 01:02:33 -0300

    limpieza codigo reflection

--- Ejemplos/Reflection/App/app/Program.cs
+++ Ejemplos/Reflection/App/app/Program.cs
@@ -18,14 +18,14 @@ namespace app
                 {
                     Console.WriteLine("Ingrese la ruta del assembly");
                     input = Console.ReadLine();
+                    //recorro el contenido del assembly, es a modo de ejemplo
+                    //para poder entender como esta estructurado un assembly y los comandos para navegarlo
                     InspectAssembly(input);
-                    Console.WriteLine("¿Que clase desea que instancie ILogger?");
-                    string aClass = Console.ReadLine();
                     Console.WriteLine("Ingrese el texto que desea guardar");
                     string toSave= Console.ReadLine();
                     Console.WriteLine("Ingrese la ruta donde desea guardar");
                     string route = Console.ReadLine();
-                    InstanceAndExecuteLog(input, aClass,toSave, route);
+                    InstanceAndExecuteLog(input, toSave, route);
 
                 }
                 catch (Exception e)
@@ -36,42 +36,15 @@ namespace app
 
         }
 
-        private static void InstanceAndExecuteLog(string input, string aClass, string toSave, string route)
+        private static void InstanceAndExecuteLog(string input, string toSave, string route)
         {
             Assembly myAssembly = Assembly.LoadFile(input);
 
-
- /*           //Caso usando la variable aClass
-            // Obtenemos el tipo que representa a User
-            Type logType = myAssembly.GetType(aClass);
-
-            // Creamos una instancia del log
-            //object logInstance = Activator.CreateInstance(actorType);
-
-            // O también podemos crearlo pasandole parámetros
-            object logInstance = Activator.CreateInstance(logType, new object[] { "LogTxt" });
-
-            // Invocamos al método
-            MethodInfo met = logType.GetMethod("Log");
-            met.Invoke(logInstance, new object[] { toSave, route });
-
-            // También podemos cambiar un atributo
-            PropertyInfo prop = logType.GetProperty("Name");
-            Console.WriteLine(prop.GetValue(logInstance)); // Antes de modificar el nombre
-            prop.SetValue(logInstance, "Txt", null);
-            Console.WriteLine(prop.GetValue(logInstance)); // Despues de cambiar el nombre
-
-            Console.ReadLine();
-*/
-
-            //El codigo debajo, no utiliza el aClass ingresado y va directamente a buscarlo a la libreria indicada
-            //De esta manera evitamos saber el nombre de la clase que implementa la interfaz
+            //Cargo todas las clases que implementen Ilogger
             IEnumerable<Type> implementations = GetTypesInAssembly<ILogger>(myAssembly);
+            //Instancio la primera de la lista(en este ejemplo la única)
             ILogger logInstance = (ILogger)Activator.CreateInstance(implementations.First());
             logInstance.Log(toSave, route);
-
-
-
         }
 
         private static IEnumerable<Type> GetTypesInAssembly<Interface>(Assembly myAssembly)
